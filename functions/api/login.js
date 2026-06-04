a32df58535fb6f271c6bf3afe34fd79c1f808486
Romantic-x <13986517961@163.com>
2026-06-04 20:00:53 +0800

    Update login.js

--- functions/api/login.js
+++ functions/api/login.js
@@ -1,5 +1,7 @@
 export async function onRequestPost({request}){
   const {user,pwd}=await request.json();
-  if(user=='admin'&&pwd=='123456') return Response.json({ok:1});
-  return Response.json({ok:0});
+  if(user=='admin'&&pwd=='123456'){
+    return Response.json({ok:1,token:'ok'})
+  }
+  return Response.json({ok:0})
 }
