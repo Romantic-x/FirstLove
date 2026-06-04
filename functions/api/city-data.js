e43c5ceb7ec12a62a21c12f20d510eb51075eb42
Romantic-x <13986517961@163.com>
2026-06-04 18:42:19 +0800

    Update city-data.js

--- functions/api/city-data.js
+++ functions/api/city-data.js
@@ -1,18 +1,20 @@
-// D1绑定变量名必须大写D1（前面CF后台绑定的变量名）
-export async function onRequestGet(context) {
-  const { D1 } = context.env;
-  // 获取前端传来的年份参数 ?year=2026
-  const year = context.request.url.searchParams.get('year') || "2026";
+export async function onRequestGet({ env, request }) {
+  try {
+    const url = new URL(request.url);
+    const year = url.searchParams.get("year") || "2026";
+    // D1查询语句
+    const { results } = await env.D1.prepare(`
+      SELECT city,population,urban_population,gdp,urbanization_rate FROM city_data WHERE year=?
+    `).bind(year).all();
 
-  // 查询指定年份全量城市数据
-  const res = await D1.prepare(`
-    SELECT year,city,population,urban_population,gdp,urbanization_rate 
-    FROM city_data WHERE year=?
-  `).bind(year).all();
-
-  return Response.json({
-    code:200,
-    year:year,
-    data:res.results
-  })
+    return Response.json({
+      cities: results.map(i=>i.city),
+      population: results.map(i=>i.population),
+      urban_population: results.map(i=>i.urban_population),
+      gdp: results.map(i=>i.gdp),
+      urbanization_rate: results.map(i=>i.urbanization_rate)
+    })
+  } catch (e) {
+    return Response.json({err:e.message}, {status:500})
+  }
 }
