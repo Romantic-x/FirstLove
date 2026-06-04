a0b67dae597fbff94dc81dbe21cbdeb92f6d5635
Romantic-x <13986517961@163.com>
2026-06-04 19:25:22 +0800

    Update city-data.js

--- functions/api/city-data.js
+++ functions/api/city-data.js
@@ -1,6 +1,6 @@
 export async function onRequestGet({ env, request }) {
   const url = new URL(request.url);
-  const year = url.searchParams.get('year')||2026;
+  const year = url.searchParams.get('year')||"2026";
   const province = url.searchParams.get('province')||'';
   const search = url.searchParams.get('search')||'';
 
@@ -12,12 +12,12 @@ export async function onRequestGet({ env, request }) {
 
   const {results} = await env.D1.prepare(query).bind(...params).all();
 
+  // 只返回图表需要的5个数组，provinces删掉，前端不需要在这里拿省份
   return Response.json({
     cities: results.map(i=>i.city),
     population: results.map(i=>i.population),
     urban_population: results.map(i=>i.urban_population),
     gdp: results.map(i=>i.gdp),
-    urbanization_rate: results.map(i=>i.urbanization_rate),
-    provinces: results.map(i=>i.province)
+    urbanization_rate: results.map(i=>i.urbanization_rate)
   })
 }
