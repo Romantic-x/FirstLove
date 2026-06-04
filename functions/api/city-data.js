ce108a74385b772cff5c2df0c352c5bf429feab4
Romantic-x <13986517961@163.com>
2026-06-04 20:17:40 +0800

    Update city-data.js

--- functions/api/city-data.js
+++ functions/api/city-data.js
@@ -2,7 +2,6 @@ export async function onRequestGet({ env, request }) {
   try {
     const url = new URL(request.url);
     const year = url.searchParams.get("year") || "2026";
-    // D1查询语句
     const { results } = await env.D1.prepare(`
       SELECT city,population,urban_population,gdp,urbanization_rate FROM city_data WHERE year=?
     `).bind(year).all();
