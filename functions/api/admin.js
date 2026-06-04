cd074b96794544413ae5cbcabab1f6fd94cc325a
Romantic-x <13986517961@163.com>
2026-06-04 20:00:33 +0800

    Update admin.js

--- functions/api/admin.js
+++ functions/api/admin.js
@@ -1,17 +1,18 @@
 export async function onRequestPost({env,request}){
-  const d = await request.json();
-  if(d.action=='add'){
-    await env.D1.prepare(`INSERT INTO city_data VALUES(NULL,?,?,?,?,?,?,?)`).bind(
-      d.year,d.province,d.city,d.population,d.urban_population,d.gdp,d.urbanization_rate
-    ).run();
+  const data=await request.json();
+  const {action,id,year,province,city,population,urban_population,gdp,urbanization_rate}=data;
+
+  if(action=='add'){
+    return await env.D1.prepare(`INSERT INTO city_data VALUES(NULL,?,?,?,?,?,?,?)`).bind(year,province,city,population,urban_population,gdp,urbanization_rate).run();
   }
-  if(d.action=='edit'){
-    await env.D1.prepare(`UPDATE city_data SET year=?,province=?,city=?,population=?,urban_population=?,gdp=?,urbanization_rate=? WHERE id=?`).bind(
-      d.year,d.province,d.city,d.population,d.urban_population,d.gdp,d.urbanization_rate,d.id
-    ).run();
+
+  if(action=='edit'){
+    return await env.D1.prepare(`UPDATE city_data SET year=?,province=?,city=?,population=?,urban_population=?,gdp=?,urbanization_rate=? WHERE id=?`).bind(year,province,city,population,urban_population,gdp,urbanization_rate,id).run();
   }
-  if(d.action=='del'){
-    await env.D1.prepare(`DELETE FROM city_data WHERE id=?`).bind(d.id).run();
+
+  if(action=='del'){
+    return await env.D1.prepare(`DELETE FROM city_data WHERE id=?`).bind(id).run();
   }
-  return Response.json({ok:1})
+
+  return Response.json({ok:0})
 }
